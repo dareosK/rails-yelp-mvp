6719faec89c31910ee52c0f41ba40945318f0a42
Dareos Khalili <khalilidareos@gmail.com>
2020-11-11 10:22:06 +0100

    adds seeds

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,3 +4,13 @@ class Restaurant < ApplicationRecord
   validates :category, inclusion: { in: CATEGORIES }
   has_many :reviews, dependent: :destroy
 end
+
+# INCLUSION validates that the attributes' values are included in a
+# given set (can be any enumerable object).
+# OPTION [:in] receives the set of accepted values.
+# The :in option has an alias => :within
+# The previous example uses the :message option to show how you can include
+# the attribute's value.
+# (For full options check the message documentation.)
+
+# The default error message for this helper is "is not included in the list".
