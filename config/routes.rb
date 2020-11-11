e693565f2163f1478708b49ceced23f19edc13c7
Dareos Khalili <khalilidareos@gmail.com>
2020-11-11 10:28:29 +0100

    adds comment with user stories corresponding to each route

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,20 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
 end
+
+# USER STORIES
+
+# A visitor can:
+# see the list of all restaurants.
+# GET "restaurants"
+
+# add a new restaurant, and be redirected to the show view of that new restaurant.
+# GET "restaurants/new"
+# POST "restaurants"
+
+# see the details of a restaurant, with all the reviews related to the restaurant.
+# GET "restaurants/38"
+
+# add a new review to a restaurant
+# GET "restaurants/38/reviews/new"
+# POST "restaurants/38/reviews"
